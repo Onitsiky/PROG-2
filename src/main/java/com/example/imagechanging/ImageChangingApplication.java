b9bf6e07ef12abacf021610043798c68fc5fab67
Onitsiky <hei.onitsiky@gmail.com>
2022-08-03 23:55:47 +0300

    java api for image treating

---
+++ src/main/java/com/example/imagechanging/ImageChangingApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagechanging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageChangingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(imageController.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imagechanging/ImageChangingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagechanging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageChangingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
